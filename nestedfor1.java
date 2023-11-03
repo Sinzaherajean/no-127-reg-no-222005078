2a83607e3c5e2cac87788ae39db76c90457b1420
Sinzaherajean <149692910+Sinzaherajean@users.noreply.github.com>
2023-11-03 15:14:33 +0200

    Add files via upload

---
+++ nestedfor1.java
@@ -0,0 +1,15 @@
+public class nestedif { // program will display numbers to see if it is negative or positive
+    public static void main(String[] args){ //declar and initialization two variables
+       int weeks = 3;
+       int days = 7;
+       for ( int i =1; i <= weeks; ++i ){
+        System.out.println("week: " + 1); //excutes the 1 up to 3
+       }
+       for (int t =1; t <= days; ++t)
+{
+    System.out.println(" day: " + t); //display 7
+}
+    }
+}
+
+    
