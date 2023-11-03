f36e3a817b830f7869d7d57d96d93e3ed4c8cd0d
Sinzaherajean <149692910+Sinzaherajean@users.noreply.github.com>
2023-11-03 14:31:13 +0200

    Add files via upload

---
+++ dowhileloop.java
@@ -0,0 +1,11 @@
+public class dowhileloop { //th is program will display counting numbers
+    public static void main(String[] args){ // the main method
+        int i=1; // variable declaration
+        do{
+            System.out.println("18"); // program will print output
+            i++; // incrementation
+        }
+        while(i<=10); // condition
+    }
+    
+}
